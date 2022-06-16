c88efd7a94d6eb03d5788c1ddbc6861b99b5ae88
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2022-06-16 15:15:30 +0200

    Added activity rotation, added time randomization

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import datetime
 import os
+import random
 
 from dotenv import load_dotenv
 
 from wicamp.app import App
-from wicamp.course import Course
+from wicamp.course import Course, TaskItemType
 from wicamp.web_driver import WebDriver
 
 load_dotenv()
@@ -16,11 +18,30 @@ def main():
     course = Course().populate()
     driver = WebDriver()
     app = App(username, password, driver)
-    app.debug()
     app.ftims_login()
-    # 2h 39min
-    app.wander(course.get_task(4))
-    # TODO: Implement time check to validate the progress
+    items = [
+        course.get_obligatory("Regulamin labo"),
+        course.get_obligatory("Regulamin przed"),
+        course.get_obligatory("Harmonogram"),
+        course.get_task(4),
+        course.get_task(4, TaskItemType.ANSWER),
+        course.get_task(4, TaskItemType.QUESTION),
+        course.get_lecture(14),
+        course.get_lecture(15)
+    ]
+    random.shuffle(items)
+    i = 0
+    while i <= len(items):
+        if i == len(items):
+            print("Mieszam w kotle, ustalam nowa kolejnosc")
+            i = 0
+            random.shuffle(items)
+            continue
+
+        item = items[i]
+        minutes = random.randint(15, 60)
+        print(f"Zabieram sie za {item.name} na {minutes} minut.")
+        app.wander(item, datetime.timedelta(minutes=minutes))
     app.close()
 
 
